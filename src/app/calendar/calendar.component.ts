4754cb98b7c6c61e10a06327d054af5535789fa3
Aneta Falkowska <aneta.falkowska@gmail.com>
2025-02-14 22:51:10 +0100

    feat(calendar): integrate FullCalendar into the app

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,11 +1,101 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+import { FullCalendarModule } from '@fullcalendar/angular';
+import { CalendarOptions } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+import interactionPlugin from '@fullcalendar/interaction';
+import { TaskService } from '../shared/task.service';
+import { Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-calendar',
-  imports: [],
+  imports: [CommonModule, RouterOutlet, FullCalendarModule],
   templateUrl: './calendar.component.html',
-  styleUrl: './calendar.component.scss'
+  styleUrl: './calendar.component.scss',
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit, OnDestroy {
+  calendarOptions!: CalendarOptions;
+  unsubscribe$ = new Subject<void>();
 
+  constructor(private readonly taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.taskService.getTasksForCalendar();
+    this.taskService.tasksForCalendar$.subscribe((calendarTasks) =>
+      this.updateCalendarOptions(calendarTasks)
+    );
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+  updateCalendarOptions(
+    calendarTasks: {
+      task: any;
+      boardName: string;
+      boardId: string;
+      columnId: string;
+    }[]
+  ): void {
+    this.calendarOptions = {
+      plugins: [dayGridPlugin, interactionPlugin],
+      initialView: 'dayGridMonth',
+      firstDay: 1,
+      events: calendarTasks.map(({ task, boardName, boardId, columnId }) => ({
+        title: `${task.name} (${boardName})`,
+        start: task.duedate,
+        backgroundColor: this.getPriorityColor(task.priority),
+        borderColor: this.getPriorityColor(task.priority),
+        id: task.id,
+        extendedProps: {
+          boardId,
+          columnId,
+        },
+      })),
+      eventStartEditable: true,
+      droppable: true,
+      eventDrop: this.handleEventDrop.bind(this),
+      eventClick: this.handleEventClick.bind(this),
+    };
+  }
+
+  getPriorityColor(priority?: string) {
+    return priority === 'high'
+      ? 'red'
+      : priority === 'medium'
+      ? 'orange'
+      : 'green';
+  }
+
+  handleEventDrop(event: any) {
+
+    const boardId = event.event.extendedProps.boardId;
+    const columnId = event.event.extendedProps.columnId;
+    const taskId = event.event.id; 
+    const newDueDate = event.event.start ? this.formatDate(event.event.start) : null;
+ 
+    if (!boardId || !columnId || !taskId || !newDueDate) return;
+
+    this.taskService
+      .updateTask(boardId, columnId, taskId, {
+        duedate: newDueDate,
+      })
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe({
+        error: (err) => console.error('Task update failed', err),
+      });
+  }
+
+  formatDate(date: Date): string {
+    return date.getFullYear() +
+    '-' + String(date.getMonth() + 1).padStart(2, '0') +
+    '-' + String(date.getDate()).padStart(2, '0');
+  }
+
+  handleEventClick(event: any) {
+    console.log(event.event.title);
+  }
 }
--- src/app/shared/task.service.ts
+++ src/app/shared/task.service.ts
@@ -3,7 +3,13 @@ import { Board } from './board.model';
 import { BoardData } from '../../db-data';
 import { Task } from './task.model';
 import { BoardService } from './board.service';
-import { catchError, map, Observable, switchMap, throwError } from 'rxjs';
+import {
+  BehaviorSubject,
+  catchError,
+  map,
+  Observable,
+  throwError,
+} from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 
 @Injectable({
@@ -11,15 +17,45 @@ import { HttpClient } from '@angular/common/http';
 })
 export class TaskService {
   private readonly API_URL = 'http://localhost:3000/api/boards';
+  private tasksSubject$ = new BehaviorSubject<
+    { task: Task; boardName: string; boardId:string; columnId:string }[]
+  >([]);
+  tasksForCalendar$ = this.tasksSubject$.asObservable();
 
   handleError(action: string) {
     return (error: any) => {
       console.error(`Error ${action}: `, error);
-      return throwError(() => new Error(`Error ${action}}`));
+      return throwError(() => new Error(`Error ${action}`));
     };
   }
 
   constructor(private http: HttpClient) {}
+
+  getTasksForCalendar(): void {
+    this.http
+      .get<Board[]>(this.API_URL)
+      .pipe(
+        map((boards) => {
+          let tasksWithDueDate: { task: Task; boardName: string; boardId:string; columnId:string }[] = [];
+
+          boards.forEach((board) => {
+            board.columns.forEach((column) => {
+              column.tasks.forEach((task) => {
+                if (task.duedate) {
+                  tasksWithDueDate.push({ task, boardName: board.name, boardId: board.id, columnId: column.id });
+                }
+              });
+            });
+          });
+           return tasksWithDueDate;
+        }),
+        catchError(this.handleError('getting tasks for calendar'))
+      )
+      .subscribe({
+        next: (tasks) => this.tasksSubject$.next(tasks),
+        error: (error) => console.error('Failed to load calendar tasks', error),
+      });
+  }
 
   getTask(boardId: string, columnId: string, taskId: string): Observable<Task> {
     return this.http
@@ -30,7 +66,6 @@ export class TaskService {
   }
 
   addTask(boardId: string, columnId: string, task: Task): Observable<Task> {
-
     return this.http
       .post<Task>(`${this.API_URL}/${boardId}/columns/${columnId}/tasks/`, task)
       .pipe(catchError(this.handleError('adding task')));
@@ -42,7 +77,6 @@ export class TaskService {
     taskId: string,
     updatedFields: Partial<Task>
   ): Observable<Task> {
-
     return this.http
       .put<Task>(
         `${this.API_URL}/${boardId}/columns/${columnId}/tasks/${taskId}`,
@@ -56,7 +90,6 @@ export class TaskService {
     columnId: string,
     taskId: string
   ): Observable<void> {
-
     return this.http
       .delete<void>(
         `${this.API_URL}/${boardId}/columns/${columnId}/tasks/${taskId}`
@@ -64,12 +97,16 @@ export class TaskService {
       .pipe(catchError(this.handleError('deleting task')));
   }
 
-  moveTask(boardId: string,
+  moveTask(
+    boardId: string,
     sourceColumnId: string,
     taskId: string,
     targetColumnId: string,
-    newIndex:number
+    newIndex: number
   ): Observable<Task> {
-    return this.http.put<Task>(`${this.API_URL}/${boardId}/columns/${sourceColumnId}/tasks/${taskId}/move/${targetColumnId}`, {newIndex} )
+    return this.http.put<Task>(
+      `${this.API_URL}/${boardId}/columns/${sourceColumnId}/tasks/${taskId}/move/${targetColumnId}`,
+      { newIndex }
+    );
   }
 }
